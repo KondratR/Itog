0bfd7ab37872c368bce3d624d41fcd7665b6b73c
Kondrat <yo126@yandex.ru>
2023-03-28 18:43:28 +0300

    zadacha

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[7] {"GeekBrains", "Privet", "656346", "lol", "Poka", "1", "02"};
+string[] array2 = new string[array1.Length];
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void FillSecond(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+FillSecond(array1, array2);
+PrintArray(array1);
+System.Console.WriteLine();
+PrintArray(array2);
